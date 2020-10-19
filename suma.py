aca730e3f35d786f757ffef54029aca92589665c
Pedro <pedro3nov@gmail.com>
2020-10-19 16:46:22 -0400

    pasando argumentos

--- suma.py
+++ suma.py
@@ -1,3 +1,7 @@
-def sum(*args):
+def suma(*args, **kwargs):
+    for a , b in kwargs.items():
+        print(a , '->>>>>' , b)
     return sum(args)
 
+if __name__ == "__main__":
+    print(suma(1 , 2  ,3 , total=6))
